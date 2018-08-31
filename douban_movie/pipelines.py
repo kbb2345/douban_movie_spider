99e738145f078e8954f22e1a0c851252669b81f0
kbb2345 <libin2345@vip.qq.com>
2018-08-31 12:04:08 +0800

    finishi

--- douban_movie/pipelines.py
+++ douban_movie/pipelines.py
@@ -9,12 +9,15 @@ import redis
 
 class DoubanMoviePipeline(object):
 	def process_item(self, item, spider):
-		print(item['name'],'*'*50)
-		if int(item['score']) >= 8:
+		print(item['url'],'*'*50)
+		if float(item['score']) >= 8:
+            '''
+			items = json.dumps({'score':item["score"],'url':
+			item['url'],'summary':item['summary'],'name':
+				item['name']})
 			print('*'*50)
-			self.redis.hmset('douban_movie:items','score',items["score"],
-				'url',item['url'],'summary',item['summary'],
-				'name',item['name'])
+            '''
+			self.redis.lpush('douban_movie:items',json.dumps(dict(item)))
 		return item
 	def open_spider(self,spider):
 		self.redis = redis.StrictRedis(host='localhost',port=6379,db=0)
--- douban_movie/spiders/awesome_movie.py
+++ douban_movie/spiders/awesome_movie.py
@@ -9,20 +9,17 @@ class AwesomeMovieSpider(CrawlSpider):
     allowed_domains = ['movie.douban.com']
     start_urls = ['https://movie.douban.com/subject/3011091/']
     rules = (
-    	Rule(LinkExtractor(allow=r'movie\.douban\.com/.*?',),
+    	Rule(LinkExtractor(allow='movie\.douban\.com/subject/\d{8}/',
+            deny=('questions',('\d{8}/\w+'))),
     		callback='parse_movie_item',follow=True),
     	)
     def parse_movie_item(self, response):
         item = MovieItem()
         item['url'] = response.url
         item['name'] = response.xpath('//h1/span[1]/text()').extract_first()
-        item['summary'] = response.xpath('//div[@id="link-report"]/span/span/text()').extract_first().strip()
+        item['summary'] = response.xpath('//div[@class="related-info"]//span[@property]/text()').extract_first()
         item['score'] = response.xpath('//div[contains(@class,"rating_self")]/strong[contains(@class,"rating_num")]/text()').extract_first()
         yield item
 
     def parse_start_url(self, response):
-        yield self.parse_movie_item(response)
-
-
-    def parse_page(self, response):
         yield self.parse_movie_item(response)
